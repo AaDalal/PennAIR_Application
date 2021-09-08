e11601ee95c241ebb812f53275a8eebe9a92bae5
AaDalal <57609353+AaDalal@users.noreply.github.com>
2021-09-08 10:01:41 -0400

    Fixed issue where some clusters were being blended into the background

--- run.py
+++ run.py
@@ -30,10 +30,13 @@ for im_name in os.listdir(in_dir):
     # plt.show()
 
     # -- DBSCAN clustering
-    clusterer = DBSCAN(eps=20*(2**.5))
-    clusters = clusterer.fit_predict(np.argwhere(im)) # Get the indices of all nonzero pixels and feed them into the dbscan algo to identify the image
+    clusterer = DBSCAN(eps=20*(2**.5), min_samples=100) # Since our images are so large, we need ot include quite a large EPS and min_samples. To make it more robust, you could sacle EPS and min_samples based on image size
+    clusters = clusterer.fit_predict(np.argwhere(im)) # Get the indices of all nonzero pixels and feed them into the dbscan algo to identify the imag
+    # Since DBSCan uses numbers -1 to represetn no cluster nad 0-n-1 for n clusters, we need to add 2 to clusters to make sure that 0, which is the base color in our thresholded image is not accidentally folded in with a cluster
+    clusters += 2 # Now unclustered points are 1, and clusters go from [2,n+1] (inclusive)
     im[np.nonzero(im)] = clusters # np.nonzero is essentially np.argwhere transposed to match what numpy indexing expects
-    plt.imshow(im, cmap = plt.get_cmap('tab10'))
+    im_show = plt.imshow(im, cmap = plt.get_cmap('tab10'))
+    plt.colorbar(im_show)
     plt.title(im_name)
     plt.show()
     
